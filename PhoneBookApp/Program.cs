4fbc7136affa8bc8c54af9b6a52d6d4e86cb502e
bozeeetopic <btopic00@fesb.hr>
2021-11-25 01:01:51 +0100

    Add function added

--- PhoneBookApp/Program.cs
+++ PhoneBookApp/Program.cs
@@ -23,43 +23,113 @@ namespace PhoneBookApp
         static void Mainfunction(Dictionary<Contact, List<Call>> contacts)
         {
             HelpingFunctions.PrintMenu();
-            var userNumberInput = NumberInput("vaš izbor", 0, 11);
+            var userNumberInput = NumberInput("vaš izbor", 0, 8);
             switch (userNumberInput)
             {
                 case 0:
-                    PrintAllContacts(contacts);
-                    break;
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    HelpingFunctions.PrintSubMenu();
-                    userNumberInput = NumberInput("vaš izbor", 0, 4);
-                    switch (userNumberInput)
                     {
-                        case 0:
-                            break;
-                        case 1:
-                            break;
-                        case 2:
-                            break;
-                        default:
-                            Console.WriteLine("Nepostojeći izbor!");
-                            break;
+                        PrintAllContacts(contacts);
+                        break;
                     }
-                    break;
+                case 1:
+                    {
+                        AddNewContact(contacts);
+                        break;
+                    }
+                case 2:
+                    {
+                        EraseContacts(contacts);
+                        break;
+                    }
+                case 3:
+                    {
+                        PrintAllContacts(contacts);
+                        break;
+                    }
+                case 4:
+                    {
+                        PrintAllContacts(contacts);
+                        break;
+                    }
+                case 5:
+                    {
+                        HelpingFunctions.PrintSubMenu();
+                        userNumberInput = NumberInput("vaš izbor", 0, 4);
+                        switch (userNumberInput)
+                        {
+                            case 0:
+                                {
+                                    PrintAllContacts(contacts);
+                                    break;
+                                }
+                            case 1:
+                                {
+                                    PrintAllContacts(contacts);
+                                    break;
+                                }
+                            case 2:
+                                {
+                                    PrintAllContacts(contacts);
+                                    break;
+                                }
+                            default:
+                                Console.WriteLine("Nepostojeći izbor!");
+                                break;
+                        }
+                        break;
+                    }
                 case 6:
-                    break;
+                    {
+                        PrintAllContacts(contacts);
+                        break;
+                    }
                 case 7:
-                    break;
+                    {
+                        PrintAllContacts(contacts);
+                        break;
+                    }
                 default:
-                    break;
+                    {
+                        PrintAllContacts(contacts);
+                        break;
+                    }
             }
+        }
+      /*  static string GetPhoneNumber()
+        {
+            var unos = "";
+            Console.WriteLine("Telefonski broj se odnosi na fiksni ili mobitel?");
+            do
+            {
+                Console.Write("Vaš odabir: ");
+                unos = Console.ReadLine();
+            } while ((unos != "fiksni") || (unos != "mobitel"));
+            if (unos == "fiksni") return GetTelephoneNumber();
+            else return GetMobilePhoneNumber();
+        }*/
+        static bool StringContainsString(string a, string b)
+        {
+            foreach (var character in b)
+            {
+                if (a.Contains(character)) return true;
+            }
+            return false;
+        }
+        static string NameOrSurnameInput(string nameOrSurname, string forbiddenString, int minLength)
+        {
+            var repeatedInput = false;
+            var input = "";
+            do
+            {
+                if (repeatedInput && (input.Length < minLength)) Console.WriteLine("Duljina " + nameOrSurname + "na mora biti "+ minLength + "!");
+                if (repeatedInput && StringContainsString(input.ToLower(), forbiddenString)) Console.WriteLine(nameOrSurname + " sadrži znak, moraju biti isključivo brojevi!");
+                Console.Write("Stanovnikovo " + nameOrSurname + ": ");
+                input = Console.ReadLine();        
+                Console.WriteLine();
+                repeatedInput = true;
+            }
+            while ((input.Length < minLength) && (StringContainsString(input.ToLower(), forbiddenString)));
+            return input;
         }
         static int NumberInput(string message, int minValue, int maxValue)
         {
@@ -67,7 +137,7 @@ namespace PhoneBookApp
             var repeatedInput = false;
             do
             {
-                if (repeatedInput) Console.WriteLine("Morate unjeti broj između " + minValue + " i " + maxValue + ".");
+                if (repeatedInput) Console.WriteLine("Morate unjeti broj između " + minValue + " i " + (maxValue-1) + ".");
                 Console.Write("Unesite " + message + ": ");
                 if (!int.TryParse(Console.ReadLine(), out number)) Console.WriteLine("Pogrešan unos, brojeve samo!");
                 repeatedInput = true;
@@ -80,7 +150,7 @@ namespace PhoneBookApp
 
             return new Dictionary<Contact, List<Call>>
             {
-            {PopulateContact("Ivo Sanader","1",0),new List<Call>(){PopulateList(DateTime.Now,0,45), PopulateList(DateTime.Now, 0,20) } }
+            {PopulateContact("Ivo Sanader","1",0),new List<Call>(){PopulateCall(DateTime.Now,0,45), PopulateCall(DateTime.Now, 0,20) } }
             };
         }
         static Contact PopulateContact(string name, string phone, Enums.Enums.PreferenceType preference)
@@ -89,12 +159,14 @@ namespace PhoneBookApp
             contact.AddValue(name, phone, preference);
             return contact;
         }
-        static Call PopulateList(DateTime time, Enums.Enums.Status status, int duration)
+        static Call PopulateCall(DateTime time, Enums.Enums.Status status, int duration)
         {
             var call = new Call();
             call.AddValue(time, status, duration);
             return call;
         }
+
+
 
         static void PrintAllContacts(Dictionary<Contact, List<Call>> contacts)
         {
@@ -103,5 +175,19 @@ namespace PhoneBookApp
                 Console.WriteLine(contact.Key.ToString());
             }
         }
+
+        static void AddNewContact(Dictionary<Contact, List<Call>> contacts)
+        {
+            Enums.Enums.PreferenceType myStatus = Enums.Enums.PreferenceType.Normal;
+            var name = NameOrSurnameInput("ime", "",1);
+            var surname = NameOrSurnameInput("prezime", "",0);
+            var number = NameOrSurnameInput("broj", "qwertzuiopšđžćčlkjhgfds ayxcvbnm,.-:;<>!#$%&/()=?*¸¨'",1);
+            Console.WriteLine("Unesi koja je ovo vrsta kontakta, za favorit 0, za default 1 te za blokirati broj upišite 2.");
+            var status = NumberInput("vaš odabir",0,3);
+            if (Enum.IsDefined(typeof(Enums.Enums.PreferenceType), status))
+                myStatus = (Enums.Enums.PreferenceType)status;
+            contacts.Add(PopulateContact(name + " " + surname, number, myStatus), null);
+        }
+
     }
 }
